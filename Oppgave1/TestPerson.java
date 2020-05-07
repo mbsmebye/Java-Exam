b3511002dedda27534b59a1267816d9e0761083c
Martin Smebye <mbsmebye@gmail.com>
2020-05-07 15:38:38 +0200

    Another small fix

--- Oppgave1/TestPerson.java
+++ Oppgave1/TestPerson.java
@@ -6,7 +6,7 @@ public class TestPerson {
         Address staffAddress = new Address("Kongens gate 1", "0001", "Norway");
 
         Person staffObject = new Staff("Ola", "Nordmann", 42, staffAddress, 50000.0);
-        Student studentObject = new Student("Espen", "Askeladd", 23);
+        Person studentObject = new Student("Espen", "Askeladd", 23);
         System.out.println(studentObject);
         System.out.println(staffObject);
         ((Staff) staffObject).setSalary(60000.0);
