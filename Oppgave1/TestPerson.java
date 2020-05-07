545a31612cfb6c0454efab4ef3c0955d9f2a795f
Martin Smebye <mbsmebye@gmail.com>
2020-05-07 15:35:37 +0200

    Small Fix

--- Oppgave1/TestPerson.java
+++ Oppgave1/TestPerson.java
@@ -5,11 +5,11 @@ public class TestPerson {
     public static void main(String[] args) {
         Address staffAddress = new Address("Kongens gate 1", "0001", "Norway");
 
-        Staff staffObject = new Staff("Ola", "Nordmann", 42, staffAddress, 50000.0);
+        Person staffObject = new Staff("Ola", "Nordmann", 42, staffAddress, 50000.0);
         Student studentObject = new Student("Espen", "Askeladd", 23);
         System.out.println(studentObject);
         System.out.println(staffObject);
-        staffObject.setSalary(60000.0);
+        ((Staff) staffObject).setSalary(60000.0);
         System.out.println(staffObject);
     }
     
